1f1784268b3b59be6a63558c56e2577016315560
Akshay v Doyizode <akshayvdoizode123@gmail.com>
2024-08-25 18:17:32 +0000

    Added code to simulate journey in real time

--- jobs/main.py
+++ jobs/main.py
@@ -5,12 +5,13 @@ from datetime import datetime, timedelta
 import random
 import uuid
 
-LONDON_COORDINATES = { "latitude": 51.5074, "longitude": -0.1278 }
-BIRMINGHAM_COORDINATES = { "latitude": 52.4862, "longitude": -1.8904 }
+LONDON_COORDINATES = { 'latitude': 51.5074, 'longitude': -0.1278 }
+BIRMINGHAM_COORDINATES = { 'latitude': 52.4862, 'longitude': -1.8904 }
+random.seed(42)
 
 # Calculate the movement increment
-LATITUDE_INCREMENT = (BIRMINGHAM_COORDINATES["latitude"] - LONDON_COORDINATES["latitude"]) / 100
-LONGITUDE_INCREMENT = (BIRMINGHAM_COORDINATES["longitude"] - LONDON_COORDINATES["longitude"]) / 100
+LATITUDE_INCREMENT = (BIRMINGHAM_COORDINATES['latitude'] - LONDON_COORDINATES['latitude']) / 100
+LONGITUDE_INCREMENT = (BIRMINGHAM_COORDINATES['longitude'] - LONDON_COORDINATES['longitude']) / 100
 
 KAFKA_BOOTSTRAP_SERVERS = os.getenv('KAFKA_BOOTSTRAP_SERVERS', 'localhost:9093')
 VEHICLE_TOPIC = os.getenv('VEHICLE_TOPIC', 'VEHICLE_TOPIC')
@@ -36,11 +37,59 @@ def simulate_vehicle_movement():
     start_location['longitude'] += random.uniform(-0.0005, 0.0005)
     return start_location
 
-def generate_vehicle_data(device_id):
+def generate_gps_data(deviceId,timestamp,vehicle_type='private'):
+    return {
+        'id':uuid.uuid4(),
+        'deviceId':deviceId,
+        'timestamp':timestamp,
+        'speed':random.uniform(0,40),
+        'direction':'NorthEast',
+        'vehicle_type':vehicle_type
+    }
+
+def generate_traffic_camera_data(deviceId,timestamp,cameraId,location):
+    return {
+        'id':uuid.uuid4(),
+        'deviceId':deviceId,
+        'cameraId':cameraId,
+        'timestamp':timestamp,
+        'location':location,
+        'snapshot':'Image',
+        
+    }
+
+def generate_weather_data(deviceId,timestamp,location):
+       return {
+        'id':uuid.uuid4(),
+        'deviceId':deviceId,
+        'timestamp':timestamp,
+        'location':location,
+        'temperature':random.uniform(-5,26),
+        'weatherCondition':random.choice(['Sunny','Cloudy','Rainy','Sonwy']),
+        'precipitation':random.uniform(0,25),
+        'windSpeed':random.uniform(0,100),
+        'humidity':random.randint(0,100),
+        'AQI':random.uniform(0,500),
+    }
+
+def generate_emergency_incident_data(deviceId,timestamp,location):
+        return {
+        'id':uuid.uuid4(),
+        'deviceId':deviceId,
+        'timestamp':timestamp,
+        'location':location,
+        'incidentId':uuid.uuid4(),
+        'type':random.choice(['Accident','Fire','Medical','Police','None']),
+        'status':random.choice(['Active','Resolved']),
+        'description':"description"
+    }
+
+    
+def generate_vehicle_data(deviceId):
     location = simulate_vehicle_movement()
     return {
         'id': uuid.uuid4(),
-        'device_id': device_id,
+        'deviceId': deviceId,
         'timestamp': get_next_time().isoformat(),
         'location': (location['latitude'], location['longitude']),
         'speed': random.uniform(10, 40),
@@ -50,27 +99,62 @@ def generate_vehicle_data(device_id):
         'year': 2024,
         'fuelType': 'Hybrid'
     }
+def json_serializer(obj):
+    if isinstance(obj,uuid.UUID):
+        return str(obj)
+    raise TypeError("Obj of type is not JSON serializable",obj.__class__.__name__)
 
-def simulate_journey(producer, device_id):
+
+def produce_to_kafka(producer,topic,data):
+    producer.produce(
+        topic,
+        key=str(data['id']),
+        value=json.dumps(data,default=json_serializer).encode('utf-8'),
+        on_delivery=delivery_report
+    )
+    
+    
+def delivery_report(err,msg):
+    if err is not None:
+        print("Failed",err)
+    else:
+        print("Success")   
+
+
+
+
+def simulate_journey(producer, deviceId):
     while True:
-        vehicle_data = generate_vehicle_data(device_id)
-        gps_data=generate_gps_data
-        print(vehicle_data)
-        # break
+        vehicle_data = generate_vehicle_data(deviceId)
+        gps_data=generate_gps_data(deviceId,vehicle_data['timestamp'])
+        traffic_cam_data=generate_traffic_camera_data(deviceId,vehicle_data['timestamp'],'Nikon-cam123',vehicle_data['location'])
+        weatherdata=generate_weather_data(deviceId,vehicle_data['timestamp'],vehicle_data['location'])
+        emergency_incident_data=generate_emergency_incident_data(deviceId,vehicle_data['timestamp'],vehicle_data['location'])
+        
+        produce_to_kafka(producer,VEHICLE_TOPIC,vehicle_data)
+        produce_to_kafka(producer,GPS_TOPIC,gps_data)
+        produce_to_kafka(producer,TRAFFIC_TOPIC,traffic_cam_data)
+        produce_to_kafka(producer,WEATHER_TOPIC,weatherdata)
+        produce_to_kafka(producer,EMERGENCY_TOPIC,emergency_incident_data)
+        break
 
 def getProducer():
     producer_config = {
         'bootstrap.servers': KAFKA_BOOTSTRAP_SERVERS,
-        'error_cb': lambda err: print("Kafka error:", err)
+        'error_cb': lambda err: print('Kafka error:', err)
     }
     producer = SerializingProducer(producer_config)
     try:
         simulate_journey(producer, 'abc started')
-        print("Simulation complete")
+        print('Simulation complete')
     except KeyboardInterrupt:
-        print("Simulation ended by the user")
+        print('Simulation ended by the user')
     except Exception as e:
-        print("Error:", e)
+        print('Error:', e)
+    finally:
+        # Ensure all messages are flushed before terminating
+        producer.flush(timeout=30)  # Wait up to 30 seconds for messages to be sent
+        print("Producer has been flushed and closed")   
 
 if __name__ == '__main__':
     getProducer()
