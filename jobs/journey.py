a3cdf259b6c323efacdb74cf75cdb656a216990e
Akshay v Doyizode <akshayvdoizode123@gmail.com>
2024-09-10 08:05:09 +0000

    updted code

--- jobs/journey.py
+++ jobs/journey.py
@@ -1,7 +1,14 @@
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType,TimestampType, DoubleType
 from pyspark.sql import SparkSession,DataFrame
 from pyspark.sql.functions import from_json,col
-from constants import * 
+import os 
+# KAFKA_BOOTSTRAP_SERVERS = os.getenv('KAFKA_BOOTSTRAP_SERVERS', 'broker:29093')
+KAFKA_BOOTSTRAP_SERVERS = os.getenv('KAFKA_BOOTSTRAP_SERVERS', 'localhost:9093')
+VEHICLE_TOPIC = os.getenv('VEHICLE_TOPIC', 'VEHICLE_TOPIC')
+GPS_TOPIC = os.getenv('GPS_TOPIC', 'GPS_TOPIC')
+TRAFFIC_TOPIC = os.getenv('TRAFFIC_TOPIC', 'TRAFFIC_TOPIC')
+WEATHER_TOPIC = os.getenv('WEATHER_TOPIC', 'WEATHER_TOPIC')
+EMERGENCY_TOPIC = os.getenv('EMERGENCY_TOPIC', 'EMERGENCY_TOPIC')
 # spark = SparkSession.builder.appName("SmartCityStreaming")\
 #         .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.13:3.5.0,",
 #                 "org.apache.hadoop:hadoop-aws:3.3.1,",
@@ -90,13 +97,18 @@ def main():
     trafficDF = read_kafka_topic(TRAFFIC_TOPIC, trafficSchema).alias('traffic')
     weatherDF = read_kafka_topic(WEATHER_TOPIC, weatherSchema).alias('weather')
     emergencyDF = read_kafka_topic(EMERGENCY_TOPIC, emergencySchema).alias('emergency')
-    
-    query1=streamWriter(vehicleDF, filepath,filepath)
-    query2 =streamWriter(gpsDF, filepath,filepath)
-    query3=streamWriter(trafficDF, filepath,filepath)
-    query4=streamWriter(weatherDF, filepath,filepath)
-    query5=streamWriter(emergencyDF, filepath,filepath)
-    query5.awaitTermination()
+    # print(vehicleDF,"vehicleDF")
+    streamWriter(vehicleDF,'s3://saprk-streaming-data-avd/checkpoints/vehicle_data','s3://saprk-streaming-data-avd/data/vehicle_data')
+    streamWriter(gpsDF,'s3://saprk-streaming-data-avd/checkpoints/gps_data','s3://saprk-streaming-data-avd/data/gps_data')
+    streamWriter(trafficDF,'s3://saprk-streaming-data-avd/checkpoints/traffic_data','s3://saprk-streaming-data-avd/data/traffic_data')
+    streamWriter(weatherDF,'s3://saprk-streaming-data-avd/checkpoints/weather_data','s3://saprk-streaming-data-avd/data/weather_data')
+    streamWriter(emergencyDF,'s3://saprk-streaming-data-avd/checkpoints/emergency_data','s3://saprk-streaming-data-avd/data/emergency_data')
+    # query1=streamWriter(vehicleDF, './parquet/checkpoints/vehicle_data', './parquet/data/vehicle_data')
+    # query2 =streamWriter(gpsDF, './parquet/checkpoints/gps_data', './parquet/data/gps_data')
+    # query3=streamWriter(trafficDF, './parquet/checkpoints/traffic_data', './parquet/data/traffic_data')
+    # query4=streamWriter(weatherDF, './parquet/checkpoints/weather_data', './parquet/data/weather_data')
+    # query5=streamWriter(emergencyDF, './parquet/checkpoints/emergency_data', './parquet/data/emergency_data')
+    # query5.awaitTermination()
     
 
 def read_kafka_topic(topic, schema):
@@ -128,5 +140,28 @@ if __name__=="__main__":
     
     
     
+  
     
+# docker exec -it   codespaces-blank-spark-master-1 spark-submit \
+# --master spark://spark-master:7077 \
+# --packages org.apache.spark:spark-sql-kafka-0-10_2.13:3.5.0,org.apache.hadoop:hadoop-aws:3.3.1,com.amazonaws:aws-java-sdk:1.11.469 \
+# jobs/journey.py
+# org.apache.spark:spark-sql-kafka-0-10_2.13:3.5.0
+
+
+# docker exec -it   codespaces-blank-spark-master-1 spark-submit --master spark://spark-master:7077 --packages org.apache.spark:spark-sql-kafka-0-10_2.13:3.5.0,org.apache.hadoop:hadoop-aws:3.3.1,com.amazonaws:aws-java-sdk:1.11.469 jobs/journey.py
+# docker exec -it codespaces-blank-spark-master-1 spark-submit \
+# --master spark://spark-master:7077 \
+# --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0,org.apache.hadoop:hadoop-aws:3.3.1,com.amazonaws:aws-java-sdk:1.11.469 \
+# jobs/journey.py
+
+
+
+
+
+
+
+
+    # Writing to folders in the same directory where the code exists
+
     
